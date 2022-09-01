b9f1c6b6ef1eb2cef264ec9f07e4ab0c76772ee0
JYOTHI1808 <112401924+JYOTHI1808@users.noreply.github.com>
2022-09-01 14:13:47 +0530

    created a program on fibonacci numbers

---
+++ project2.py
@@ -0,0 +1,14 @@
+  PROJECT ON LOOPS
+         
+         //PYTHON PROGRAM FOR FIBONACCI NUMBERS
+  def fibonacci(n):
+    a=0
+    b=1
+    for i in range(0,n):
+      temp = a
+      a = b
+      b =temp +b
+    return a
+
+ for c in range(0, 13):
+    print(fibonacci(c))
