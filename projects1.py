c630bc9ef40e3ad594a13ea9b4516d10ef9c9b45
JYOTHI1808 <112401924+JYOTHI1808@users.noreply.github.com>
2022-09-03 16:31:58 +0530

    created a program on files

---
+++ projects1.py
@@ -0,0 +1,7 @@
+#PYTHON PROGRAM TO ACCEPT A FILENAME FROM THE USER AND PRINT THE EXTENSION OF THAT
+
+
+r = float(input("input the radius of the circle : "))
+pi = 3.14159
+print("the area of the circle with radius"+str(r) +"is :"+ str (pi*r**2))
+
