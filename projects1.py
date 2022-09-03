51fd0d1f52ad04a2c68beb4fa9ab86d5cc743961
JYOTHI1808 <112401924+JYOTHI1808@users.noreply.github.com>
2022-09-03 16:46:44 +0530

    created program on files

--- projects1.py
+++ projects1.py
@@ -1,7 +1,5 @@
 #PYTHON PROGRAM TO ACCEPT A FILENAME FROM THE USER AND PRINT THE EXTENSION OF THAT
-
-
-r = float(input("input the radius of the circle : "))
-pi = 3.14159
-print("the area of the circle with radius"+str(r) +"is :"+ str (pi*r**2))
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
 
